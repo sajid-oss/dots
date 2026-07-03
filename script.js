e89729b49e4a541c7dbe5d998a2a24ea783a728a
sajid-oss <sajidmehmoodkhan488@gmail.com>
2026-07-04 00:32:25 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-document.getElementById('cta-btn').addEventListener('click', () => {
+function convertImageToPDF() {
+    const { jsPDF } = window.jspdf;
+    const input = document.getElementById('imageInput');
+    
+    if (input.files && input.files[0]) {
+        const reader = new FileReader();
+        reader.onload = function (e) {
+            const doc = new jsPDF();
+            doc.addImage(e.target.result, 'JPEG', 10, 10, 180, 150);
+            doc.save("Pageup-Converted.pdf");
+        };
+        reader.readAsDataURL(input.files[0]);
+    } else {
+        alert("Pehle image select karein!");
+    }
+}document.getElementById('cta-btn').addEventListener('click', () => {
     alert('Welcome to our premium service!');
 });
